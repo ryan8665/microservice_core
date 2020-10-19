24b31f66390021c7ce1aaab6ba9c0b698898285b
Ryan <amirkhany@gmail.com>
2020-10-19 16:19:41 +0330

    actuator added and BaseException is changed

--- src/main/java/com/ns/core/CoreApplication.java
+++ src/main/java/com/ns/core/CoreApplication.java
@@ -2,7 +2,9 @@ package com.ns.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CoreApplication {
 
--- src/main/java/com/ns/core/excptions/BaseException.java
+++ src/main/java/com/ns/core/excptions/BaseException.java
@@ -1,4 +1,18 @@
 package com.ns.core.excptions;
 
 public class BaseException extends RuntimeException{
+    public BaseException() {
+    }
+
+    public BaseException(String var1) {
+        super(var1);
+    }
+
+    public BaseException(String var1, Throwable var2) {
+        super(var1, var2);
+    }
+
+    public BaseException(Throwable var1) {
+        super(var1);
+    }
 }
